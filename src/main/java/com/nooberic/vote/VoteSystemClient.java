11c50b4ecfdb56be2c3a906e685b465be5deb26d
Badnuker <badnuker@126.com>
2025-07-27 11:48:51 +0800

    Change basic info

---
+++ src/main/java/com/nooberic/vote/VoteSystemClient.java
@@ -0,0 +1,11 @@
+package com.nooberic.vote;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoteSystemClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
